394a6ab669d6fc4b436581155e17ee4d8d90774a
manohar <manumanohar1527@gmail.com>
2020-04-13 12:32:36 +0530

    getting data

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 let playArea = {};
 let player = {};
-
+let gameObj;
 
 playArea.game = document.querySelector(".game");
-
 playArea.main = document.querySelector(".main");
-
 playArea.page = document.querySelector(".page");
-
 playArea.btn = Array.from(document.querySelectorAll(".btn"));
-
 playArea.page = document.querySelectorAll(".page")
-
 playArea.status = document.querySelector(".status");
 console.log(playArea);
 
@@ -19,6 +14,26 @@ player.score = 0;
 player.items = 3;
 
 console.log(player);
+
+//event fires when all the DOM contents loads..
+document.addEventListener("DOMContentLoaded", getData);
+//function which fetchs the data
+function getData() {
+    try {
+        fetch("https://cors-anywhere.herokuapp.com/https://api.myjson.com/bins/gungm").then(function (rep) {
+            if (!rep.ok) throw new Error(rep.statusText);
+            return rep.json();
+        }).then(function (data) {
+            console.log(data);
+            gameObj = data.data;
+            console.log(gameObj);
+        })
+    } catch (error) {
+        console.log("it's not fetching the data please check the API and make sure the code is running succesfully..")
+    }
+
+    console.log("DOM loaded");
+}
 
 //looping through the button and setting up the eventListnerss..
 
